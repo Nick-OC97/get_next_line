b8135629559460864f628b2c104f8b86a12e37ab
Nicholas O'CONNELL <no-conne@c4r14s6.wethinkcode.co.za>
2019-06-06 17:23:32 +0200

    started bare bones

---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: no-conne <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/06 08:36:00 by no-conne          #+#    #+#             */
+/*   Updated: 2019/06/06 14:28:18 by no-conne         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include "libft.h"
+#include <stdlib.h>
+#include <unistd.h>
+
+#define BUFF_SIZE 32
+
+int		get_next_line(const int fd, char **line);
+
+#endif
